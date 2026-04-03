4d6338aa99ee83e8ef3b9fabbf3a5f1a78da776d
varsik222 <varsikaharonyan0@gmail.com>
2026-04-03 11:48:34 +0400

    Add files via upload

---
+++ Room2/Coding/team16_py.py
@@ -0,0 +1,16 @@
+def primes(numbers):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+    primes = []
+    for num in numbers:
+        if is_prime(num):
+            primes.append(num)
+    
+    return primes
+    
