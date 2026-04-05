1eae0fbf60b9986a0737dfea14b27b47f02848cd
Anna <arutiuniananna7@gmail.com>
2026-04-05 18:08:34 +0400

    added room2 python code test

--- Room2/Coding/team16_py.py
+++ Room2/Coding/team16_py.py
@@ -13,4 +13,8 @@ def primes(numbers):
             primes.append(num)
     
     return primes
+
+if __name__ == "__main__":
+    prime_numbers = primes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    print(prime_numbers)
     
