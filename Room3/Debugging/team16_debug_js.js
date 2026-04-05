4f9d57619a7bc0c9a2b5f7a092da121c6d4acce3
Eliza <101627185+Elizavs@users.noreply.github.com>
2026-04-06 00:23:09 +0400

    Add files via upload

---
+++ Room3/Debugging/team16_debug_js.js
@@ -0,0 +1,11 @@
+//Fix the code so that each API fetch logs the correct URL and data
+
+function fetchData(urls) {
+  for (let i = 0; i < urls.length; i++) {
+    fetch(urls[i])
+      .then(response => response.json())
+      .then(data => console.log('Data from', urls[i], data));
+  }
+}
+
+fetchData(['api/1', 'api/2', 'api/3']);
