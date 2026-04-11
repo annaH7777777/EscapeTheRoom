0410032faa628c71173afc2379bae9fbc20fed97
Eliza <101627185+Elizavs@users.noreply.github.com>
2026-04-12 01:41:43 +0400

    Update team16_coding_js.js

--- Room6/Coding/team16_coding_js.js
+++ Room6/Coding/team16_coding_js.js
@@ -1,1 +1,10 @@
-For JavaScript: Write a function that creates a Promise which resolves after a given delay (in ms).#
+//For JavaScript: Write a function that creates a Promise which resolves after a given delay (in ms).#
+
+ function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Example usage:
+delay(2000).then(() => {
+  console.log("Resolved after 2 seconds");
+});
